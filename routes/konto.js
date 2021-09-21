12efb00ff7569962095fe95b8880733e26af95c7
Adamastos3 <chseifriz3@gmail.com>
2021-09-21 18:02:57 +0200

    add new routes

--- routes/konto.js
+++ routes/konto.js
@@ -8,9 +8,34 @@ server.get("/konto", (req, res) => {
   res.sendFile("konto.html", { root: path.join(__dirname, "..", "view") });
 });
 
-server.get("/konto/:id", (req, res) => {
-  console.log("test");
+server.get("/neueskonto", (req, res) => {
+  res.sendFile("newKonto.html", { root: path.join(__dirname, "..", "view") });
+});
+
+server.get("/updatekonto", (req, res) => {
+  res.sendFile("updateKonto.html", {
+    root: path.join(__dirname, "..", "view"),
+  });
+});
+
+server.get("/konto/haushaltsbuch/:id", (req, res) => {
   konto.getKontoByHaushaltsbuchID(req, res);
+});
+
+server.get("/konto/:id", (req, res) => {
+  konto.getKontoById(req, res);
+});
+
+server.post("/konto", (req, res) => {
+  konto.addKonto(req, res);
+});
+
+server.put("/konto", (req, res) => {
+  konto.updateKonto(req, res);
+});
+
+server.delete("/konto/:id", (req, res) => {
+  konto.deleteKonto(res, req);
 });
 
 module.exports = server;
