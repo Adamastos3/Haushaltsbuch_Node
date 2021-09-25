9547b650d559431a8a66bdc0356057d902b21592
Adamastos3 <chseifriz3@gmail.com>
2021-09-25 12:07:03 +0200

    add routes ausgaben

---
+++ routes/ausgaben.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const server = express();
+const ausgaben = require("../service/ausgaben");
+
+//Ausgaben
+server.get("/ausgaben", (req, res) => {
+  res.sendFile("ausgaben.html", { root: path.join(__dirname, "..", "view") });
+});
+
+server.get("/neueAusgabe", (req, res) => {
+  res.sendFile("newAusgabe.html", {
+    root: path.join(__dirname, "..", "view"),
+  });
+});
+
+server.get("/updateAusgabe", (req, res) => {
+  res.sendFile("updateAusgabe.html", {
+    root: path.join(__dirname, "..", "view"),
+  });
+});
+
+server.get("/ausgaben/sort", (req, res) => {
+  ausgaben.getAusgabenBySort(req, res);
+});
+
+server.get("/ausgaben/:id", (req, res) => {
+  ausgaben.getAusgabenById(req, res);
+});
+
+server.post("/ausgaben", (req, res) => {
+  ausgaben.addAusgaben(req, res);
+});
+
+server.put("/ausgaben", (req, res) => {
+  ausgaben.updateAusgaben(req, res);
+});
+
+server.delete("/ausgaben/:id", (req, res) => {
+  ausgaben.deleteAusgaben(res, req);
+});
+
+module.exports = server;
